b0008a2e704a62c24664f32887854057cb9e8350
nightmare <suchitraswain.2012@gmail.com>
2020-09-25 18:47:54 +0530

    hard code

--- tinder-frontend/src/TinderCards.js
+++ tinder-frontend/src/TinderCards.js
@@ -4,17 +4,55 @@ import "./TinderCards.css";
 import axios from "./axios";
 
 function TinderCards() {
-  const [people, setPeople] = useState([]);
+  // const [people, setPeople] = useState([]);
 
-  useEffect(() => {
-    async function fetchData() {
-      const req = await axios.get("/tinder/cards");
-      console.log(req);
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const req = await axios.get("/tinder/cards");
+  //     console.log(req);
 
-      setPeople(req.data);
-    }
-    fetchData();
-  }, [people]);
+  //     setPeople(req.data);
+  //   }
+  //   fetchData();
+  // }, [people]);
+
+  const [people, setPeople] = useState([
+    {
+      name: "Brooks Leibee",
+      imgUrl:
+        "https://images.unsplash.com/photo-1518806118471-f28b20a1d79d?ixlib=rb-...",
+    },
+    {
+      name: "Mia Dam",
+      imgUrl:
+        "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-...",
+    },
+    {
+      name: "Michael henry",
+      imgUrl:
+        "https://images.unsplash.com/photo-1497316730643-415fac54a2af?ixlib=rb-...",
+    },
+    {
+      name: "Ben Parker",
+      imgUrl:
+        "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-...",
+    },
+    {
+      name: "Halama Steve",
+      imgUrl:
+        "https://images.unsplash.com/photo-1520155707862-5b32817388d6?ixlib=rb-...",
+    },
+    {
+      name: "Lesly Juarez",
+      imgUrl:
+        "https://images.unsplash.com/photo-1521572267360-ee0c2909d518?ixlib=rb-...",
+    },
+    {
+      name: "Gian Cescon",
+      imgUrl:
+        "https://images.unsplash.com/photo-1524250502761-1ac6f2e30d43?ixlib=rb-...",
+    },
+  ]);
 
   const swiped = (direction, nameToDelete) => {
     console.log("removing" + nameToDelete);
--- tinder-frontend/src/axios.js
+++ tinder-frontend/src/axios.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: "http://localhost:8001",
+  // baseURL:"https://git.heroku.com/tinder-backend-mern-clone.git"
 });
 
 export default instance;
